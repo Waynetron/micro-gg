7965f4ccb214a7d5bc0fb00183de3f72fe349488
Waynetron <wpetzler@gmail.com>
2019-02-04 18:13:39 +1100

    Added local persistence

--- src/index.js
+++ src/index.js
@@ -2,19 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
-import reducers from './reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+import rootReducers from './reducers'
 import './index.css';
 import './fonts/FiraCode/fira_code.css';
 import App from './App/App.js';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
 const store = createStore(
-  reducers, /* preloadedState, */
+  persistedReducer, /* preloadedState, */
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
  );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
