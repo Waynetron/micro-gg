77b94547a0e80a3dcad212d5db723e439dd37626
Waynetron <wpetzler@gmail.com>
2019-03-27 09:25:09 +1100

    Replaced square brackets in initial state

--- src/Code/reducer.js
+++ src/Code/reducer.js
@@ -17,27 +17,27 @@ P = Player
 G = Goomba
 
 // Accelerate down, like gravity
-[ Player ] -> [ DOWN Player ]
-[ Goomba ] -> [ DOWN Goomba ]
+{ Player } -> { DOWN Player }
+{ Goomba } -> { DOWN Goomba }
 
 // Move sideways with <LEFT> and <RIGHT> arrow keys
-[ <HORIZONTAL> Player ] -> [ HORIZONTAL Player ]
+{ <HORIZONTAL> Player } -> { HORIZONTAL Player }
 
 // Jump when <UP> is pressed and when the bottom of Player
 // is touching Wall (Wall = anything)
-DOWN [ <UP> Player | Wall ] -> [ JUMP Player | Wall ]
+DOWN { <UP> Player | Wall } -> { JUMP Player | Wall }
 
 // Player dies if touching Goomba on the side
-HORIZONTAL [ Player | Goomba ] -> [ DEAD Player | Goomba ]
+HORIZONTAL { Player | Goomba } -> { DEAD Player | Goomba }
 
 // Jumping on top of Goomba kills the Goomba
-DOWN [ Player | Goomba ] -> [ Player | DEAD Goomba ]
+DOWN { Player | Goomba } -> { Player | DEAD Goomba }
 
 // Break bricks by head-butting
-UP [ Player | Brick ] -> [ Player | DEAD Brick ]
+UP { Player | Brick } -> { Player | DEAD Brick }
 
 // Head-butting QuestionBrick turns it into another Player
-UP [ Player | QuestionBrick ] -> [ Player | JUMP Player ]
+UP { Player | QuestionBrick } -> { Player | JUMP Player }
 `;
 
 const defaultState = {
