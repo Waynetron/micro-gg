f2306da5a78129bf6a364ee893edb7fa536dcd10
Waynetron <wpetzler@gmail.com>
2019-05-20 19:18:44 +1000

    Replaced Material UI Tooltip with custom implementation

--- src/ImagePicker/ImagePicker.js
+++ src/ImagePicker/ImagePicker.js
@@ -1,47 +1,70 @@
-import React from 'react'
+import React, {useState, Fragment} from 'react'
 import {connect} from 'react-redux'
-import Tooltip from '@material-ui/core/Tooltip'
+import {debounce} from 'lodash';
 import LetterImage from '../Image/LetterImage'
 import {getImageFilename} from '../Image/helper'
 
+import styled from 'styled-components'
+
+const PickerContainer = styled.div`
+  position: absolute;
+  display: inline-flex;
+  cursor: pointer;
+  margin-top: -0.5rem;
+  z-index: 1;
+`
+
+const PickerBox = styled.div`
+  display: flex;
+  background-color: black;
+  border: solid 1px #555;
+  border-radius: .5rem;
+  padding: 0.5rem;
+  margin-top: -0.5rem;
+
+  div, img {
+    margin-right: 0.5rem;
+    
+    &:last-child {
+      margin-right: 0
+    }
+  }
+`
 
 const ImagePicker = ({letter, variableName, imageMap, availableImages, onSetImage, onSetNoImage})=> {
   const imageFilename = getImageFilename(variableName, imageMap, availableImages)
+  const [open, setOpen] = useState(false)
 
   return (
-    <Tooltip
-      interactive
-      className='tooltip'
-      title={
-        <div className='tooltip-content'>
-          <LetterImage
-            letter={letter}
-            onClick={()=> onSetNoImage(variableName)}
-          />
-          {availableImages.map((imageName)=> (
-            <img
-              key={imageName}
-              alt=''
-              src={require(`../Game/images/${imageName}.png`)}
-              onClick={()=> onSetImage(variableName, imageName)}
-            />
-          ))}
-        </div>
-      }
+    <PickerContainer
+      onMouseOver={()=> setOpen(true)}
+      onMouseLeave={debounce(()=> setOpen(false), 250)}
     >
-      {imageFilename
-        ? 
-          <img
-            className='code-sprite'
-            alt=''
-            src={require(`../Game/images/${imageFilename}.png`)}
-          />
-        : <LetterImage
-            className='code-sprite'
-            letter={letter}
-          />
+      {!open
+        ?
+          <div className='code-sprite'>
+            {imageFilename
+              ? <img alt='' src={require(`../Game/images/${imageFilename}.png`)} />
+              : <LetterImage letter={letter} />
+            }
+          </div>
+        : 
+          <PickerBox>
+            <LetterImage
+              letter={letter}
+              onClick={()=> onSetNoImage(variableName)}
+            />
+            {availableImages.map((imageName)=> (
+              <img
+                key={imageName}
+                alt=''
+                src={require(`../Game/images/${imageName}.png`)}
+                onClick={()=> onSetImage(variableName, imageName)}
+              />
+            ))}
+          </PickerBox>
       }
-    </Tooltip>
+    </PickerContainer>
   )
 };
 
